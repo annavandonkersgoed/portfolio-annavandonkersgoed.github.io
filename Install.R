0a535aaca4f8a37956291a1ef8ba4a59d467e929
Ghislain Westphal <anna.vandonkersgoed@student.hu.nl>
2024-07-08 15:18:28 +0200

    Eigen ruimte

---
+++ Install.R
@@ -0,0 +1,9 @@
+#Install these packages if you want to build the bookdown
+
+install.packages("here")
+install.packages("bookdown")
+install.packages("vitae")
+install.packages("ggplot2")
+install.packages("readxl")
+install.packages("tidyverse")
+install.packages("gridExtra")
